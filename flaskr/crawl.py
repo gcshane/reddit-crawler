e4a9ccc82e050213097cb179331e0e340a1c2ba3
gcshane <gcshane@gmail.com>
2025-05-19 15:53:15 +0800

    Save top posts to database

--- flaskr/crawl.py
+++ flaskr/crawl.py
@@ -1,23 +1,34 @@
 from flask import Blueprint
-import praw, os
 from dotenv import load_dotenv
+from datetime import datetime
+import praw, os, pymongo
 
 load_dotenv()
 
 bp = Blueprint('main', __name__)
 
-@bp.route('/crawl/<subreddit>/<int:top>', methods=['GET'])
-def crawl(subreddit, top):
+@bp.route('/crawl/<subreddit_name>/<int:top>', methods=['GET'])
+def crawl(subreddit_name, top):
+        
     reddit = praw.Reddit(
         client_id = os.environ.get('client_id'),
         client_secret = os.environ.get('client_secret'),
         user_agent = os.environ.get('user_agent')
     )
-    subreddit = reddit.subreddit(subreddit)
-    top_posts = subreddit.top(time_filter='day', limit=top)
-    return [{
+    sub_reddit = reddit.subreddit(subreddit_name)
+    top_posts = sub_reddit.top(time_filter='day', limit=top)
+
+    result = [{
         "title": post.title,
         "score": post.score,
         "url": post.url,
         "permalink": post.permalink
     } for post in sorted(top_posts, key=lambda x: x.score, reverse=True)]
+
+    document = {"crawl_time" : datetime.now() ,"items" : result}
+    myclient = pymongo.MongoClient(os.environ.get('mongo_uri'))
+    mydb = myclient["reddit_crawler"]
+    mycol = mydb[subreddit_name]
+    mycol.insert_one(document)
+
+    return result
